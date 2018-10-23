be3ff8f7231f90de012a3bc66257c15b420391f2
Khalifa <m_khalifa@tss-hr420-dm08.fc.ca>
2018-10-23 13:32:44 -0400

    corrected errors

--- RPSGame.py
+++ RPSGame.py
@@ -2,11 +2,11 @@
 from random import randint
 
 # choices is as array => a container that can hold multiple items
-# arrays are 0-based => the first item is 0, the second is 1, etc 
+# arrays are 0-based => the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
 
 # make the computer choose a weapon from the coices array at random
-computer_choice = choices[randint(0,2)]
+computer_choice = choices[randint(0, 2)]
 
-#print the choice to the terminal window
-print("Computer chooses: ",computer_choice)
+# print the choice to the terminal window
+print("Computer chooses: ", computer_choice)
