9b6161c344b6b39767ca6c5a2afb5c84a115d25b
Khalifa <m_khalifa@tss-hr420-dm08.fc.ca>
2018-10-23 13:29:17 -0400

    modified

--- RPSGame.py
+++ RPSGame.py
@@ -2,7 +2,7 @@
 from random import randint
 
 # choices is as array => a container that can hold multiple items
-# arrays are 0-based => th efirst item is 0, the second is 1, etc 
+# arrays are 0-based => the first item is 0, the second is 1, etc 
 choices = ["Rock", "Paper", "Scissors"]
 
 # make the computer choose a weapon from the coices array at random
